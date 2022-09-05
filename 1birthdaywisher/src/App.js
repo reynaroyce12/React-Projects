0d0a97863fbc60a89fa7d497e1090afbe792ff1a
Reyna <reynaroyce.12@gmail.com>
2022-09-05 23:54:22 +0530

    initial

---
+++ 1birthdaywisher/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import List from "./List";
+import data from "./data";
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
